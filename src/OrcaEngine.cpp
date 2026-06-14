844a517bbc968fcf51b1f30de1bb0d0ba0a39c32
Moises Castro <moisescastro112@gmail.com>
2026-06-13 23:00:38 -0400

    Initial commit

---
+++ src/OrcaEngine.cpp
@@ -0,0 +1,12 @@
+﻿// OrcaEngine.cpp : Defines the entry point for the application.
+//
+
+#include "OrcaEngine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
